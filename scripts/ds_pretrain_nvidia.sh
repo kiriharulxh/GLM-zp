03fd9dfcd9adf557f892d89eadf49e2be7548685
liuxinghan <liuxinghan99@163.com>
2023-06-28 16:09:53 +0800

    multinode

--- scripts/ds_pretrain_nvidia.sh
+++ scripts/ds_pretrain_nvidia.sh
@@ -2,7 +2,7 @@
 
 # Change for multinode config
 
-NUM_WORKERS=32
+NUM_WORKERS=3
 NUM_GPUS_PER_WORKER=8
 MP_SIZE=1
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
@@ -11,7 +11,7 @@ source $1
 DATESTR=$(date +"%m-%d-%H-%M")
 
 OPTIONS_NCCL="NCCL_DEBUG=info NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=2"
-HOST_FILE_PATH="/workspace/hostfile"
+HOST_FILE_PATH="/share/lxh/distribute_train/GLM-zp/hostfile"
 
 mkdir logs
 run_cmd="${OPTIONS_NCCL} deepspeed --master_port ${MASTER_PORT} --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} pretrain_glm.py ${gpt_options} 2>&1 | tee logs/log-${DATESTR}.txt"
