e21cbf78c8a016c66bba15ea650a25bafc8edb76
liuxinghan <liuxinghan99@163.com>
2023-07-09 14:18:52 +0800

    eval heldin

---
+++ scripts/evaluate_seq2seq-heldin.sh
@@ -0,0 +1,40 @@
+DATA_ROOT=/root/data
+CHECKPOINT_PATH="/share/lxh"
+TASK_NAME=customization
+DATA_PATH="/share/lxh/distribute_train/train_data_zs/val.jsonl"
+DATESTR=$(date +"%m-%d-%H-%M")
+
+source $1    # Model
+
+NUM_WORKERS=1
+NUM_GPUS_PER_WORKER=1
+HOST_FILE_PATH="./hostfile"
+MP_SIZE=1
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+MAX_SEQ_LEN=2048
+
+OPTIONS_NCCL="NCCL_DEBUG=info NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=2"
+DISTRIBUTED_ARGS="${OPTIONS_NCCL} deepspeed --hostfile ${HOST_FILE_PATH} --master_port ${MASTER_PORT} --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER}"
+
+EXPERIMENT_NAME=${EXPERIMENT_NAME}_${DATESTR}
+mkdir logs
+run_cmd="${DISTRIBUTED_ARGS} finetune_glm.py \
+       --deepspeed \
+       --deepspeed_config config_tasks/config_blocklm_10B_cnndm.json \
+       --finetune \
+       --task ${TASK_NAME} \
+       --test-data ${DATA_PATH} \
+       --seq-length ${MAX_SEQ_LEN} \
+       --checkpoint-activations \
+       --num-workers 1 \
+       --no-load-optim \
+       --no-load-lr-scheduler \
+       $MODEL_ARGS \
+       --fp16 \
+       --model-parallel-size ${MP_SIZE} \
+       --epochs 0 \
+       --overwrite \
+       2>&1"
+
+echo ${run_cmd}
+eval ${run_cmd}
