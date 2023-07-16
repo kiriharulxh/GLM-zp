fe392705cabf4efa63c5d233a4871f2ab0ade222
liuxinghan <liuxinghan99@163.com>
2023-07-16 16:14:07 +0800

    ckpt

--- scripts/evaluate_chinese-heldout-mc.sh
+++ scripts/evaluate_chinese-heldout-mc.sh
@@ -1,11 +1,11 @@
 DATESTR=$(date +"%m-%d-%H-%M")
 
-# nohup bash scripts/evaluate_chiflan-heldout-0.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout0-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-1.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout1-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-2.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout2-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-3.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout3-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-4.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout4-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-5.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout5-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-6.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout6-${DATESTR}.txt &
-nohup bash scripts/evaluate_chiflan-heldout-7.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout7-${DATESTR}.txt &
+nohup bash scripts/evaluate_chiflan-heldout-0.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout0-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-1.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout1-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-2.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout2-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-3.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout3-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-4.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout4-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-5.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout5-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-6.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout6-${DATESTR}-chinese.txt &
+nohup bash scripts/evaluate_chiflan-heldout-7.sh config_tasks/model_blocklm_10B_chinese.sh &> eval/heldout7-${DATESTR}-chinese.txt &
 
